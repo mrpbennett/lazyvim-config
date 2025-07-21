650605e094ce8187b9af51765ed37b45383cf08c
mrpbennett <pbennett.uk@gmail.com>
2025-07-21 17:13:22 +0100

    update plugin configurations and keymaps; add Copilot support and Python linter options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-
+-- python liniter
 vim.g.lazyvim_python_ruff = "ruff"
+
+-- relative numbers
+vim.wo.relativenumber = true
