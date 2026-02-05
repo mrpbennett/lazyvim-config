f616d7ef75e8132af7191709786c02d72cb5dee1
Paul Bennett <pbennett.uk@outlook.com>
2026-02-05 09:06:04 +0000

    lualine clean up

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,8 @@
 vim.keymap.set("i", "jj", "<Esc>", { noremap = false })
 vim.keymap.set("i", "jk", "<Esc>", { noremap = false })
 
-vim.keymap.set("n", "aa", "A", { noremap = false }) -- Pressing 'aa' in normal mode will take you to the end of the line in insert mode
+-- Pressing 'aa' in normal mode will take you to the end of the line in insert mode
+vim.keymap.set("n", "aa", "A", { noremap = false })
 
 -- autoindent in insert mode
 vim.keymap.set("i", "<CR", "<C-o>0<CR>", { noremap = true })
@@ -20,14 +21,7 @@ vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down (insert mode)" })
 vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up (insert mode)" })
 
--- k9s
-if vim.fn.executable("k9s") == 1 then
-  vim.keymap.set("n", "<leader>k9", function()
-    Snacks.terminal("k9s")
-  end, { desc = "K9s (Kubernetes)" })
-end
-
--- k9s
+-- Pop a terminal window with K9s
 if vim.fn.executable("k9s") == 1 then
   vim.keymap.set("n", "<leader>k9", function()
     Snacks.terminal("k9s")
