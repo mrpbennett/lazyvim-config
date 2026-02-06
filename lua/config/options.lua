23b1964ae83925fdefeaf65896762a6800dd28c0
Paul Bennett <pbennett.uk@outlook.com>
2026-02-06 09:42:41 +0000

    stopping auto comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
+-- Stop auto comments
+vim.opt.formatoptions:remove({ "c", "r", "o" })
+
+-- Python
 vim.g.lazyvim_python_lsp = "ty"
 vim.g.lazyvim_python_ruff = "ruff"
 
