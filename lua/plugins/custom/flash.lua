0743f09b3fc5c745e09a7414398482f8cc6130f9
Paul Bennett <pbennett.uk@outlook.com>
2026-03-08 17:04:53 +0000

    adding custom flash config
    allow f,F / t,T jumps with a jump char

---
+++ lua/plugins/custom/flash.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  --@type Flash.Config
+  opts = {
+    modes = {
+      -- search = {
+      --   enabled = true,
+      -- },
+      char = {
+        jump_labels = true,
+      },
+    },
+  },
+}
